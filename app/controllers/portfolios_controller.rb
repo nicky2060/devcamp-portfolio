199d3126d39585110df7be15cfe7147a4b8ce66a
Nicholas Pimentel <nick@swiftmedical.io>
2020-05-30 20:37:00 -0400

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,10 +32,20 @@ class PortfoliosController < ApplicationController
         format.json { render :show, status: :ok, location: @portfolio_item }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end 
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
