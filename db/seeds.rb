76ddf8e33b30701eb0de495242366b1a67148a84
Nicholas Pimentel <nick@swiftmedical.io>
2020-06-28 09:29:52 -0400

    Added new technology item

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,12 @@ end
 end 
 
 puts "9 portfolio items created."
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+
+ puts "3 technologies created."
+ 
+end 
